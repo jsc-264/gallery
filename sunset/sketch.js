c3f770c670775de8905464d3f05fdac103bed6da
jsc-264 <joestephen264@gmail.com>
2026-04-15 22:19:20 +0100

    scene: created drawTile function

--- sunset/sketch.js
+++ sunset/sketch.js
@@ -1,6 +1,14 @@
 const DIMENSION = 10
 let tileWidth
 
+function drawTile(x, y, w){
+  push()
+  translate(x, y)
+  noFill()
+  circle(0, 0, w)
+  pop()
+}
+
 function setup() {
   createCanvas(400, 400);
   tileWidth = width / DIMENSION
@@ -11,7 +19,9 @@ function draw() {
 
   for (let j = 0; j < DIMENSION; j++) {
     for(let i = 0; i < DIMENSION; i++) {
-      rect(tileWidth * i, tileWidth * j, tileWidth)
+      const centerX = tileWidth * i + tileWidth / 2
+      const centerY = tileWidth * j + tileWidth / 2
+      drawTile(centerX, centerY, tileWidth)
     }
   }
 }
