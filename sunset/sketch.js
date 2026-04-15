6f7e9ee336feef6d0a07a5394a4801916764a920
jsc-264 <joestephen264@gmail.com>
2026-04-15 22:45:23 +0100

    sunset: created circle gradient

--- sunset/sketch.js
+++ sunset/sketch.js
@@ -1,27 +1,53 @@
 const DIMENSION = 10
 let tileWidth
 
-function drawTile(x, y, w){
+let startColour, endColour
+
+function drawTile(tileX, tileY, tileJ, tileWidth){
   push()
-  translate(x, y)
+  translate(tileX, tileY)
+
   noFill()
-  circle(0, 0, w)
+
+  const circleWeight = map(tileJ, 0, DIMENSION, 2, 1)
+  strokeWeight(circleWeight)
+
+  const colT = map(tileJ, 0, DIMENSION, 0, 1)
+  const col = lerpColor(startColour, endColour, colT)
+  stroke(col)
+
+  const range = map(tileJ, 0, DIMENSION, tileWidth/3, tileWidth/10)
+
+  const numCircles = map(tileJ, 0, DIMENSION, 10, 0)
+  for (let i = 0; i < numCircles; i++){
+    const circleX = random(-range, range)
+    const circleY = random(-range, range)
+    circle(circleX, circleY, tileWidth)
+  }
+
+
+  circle(0, 0, tileWidth)
   pop()
 }
 
 function setup() {
   createCanvas(400, 400);
   tileWidth = width / DIMENSION
+
+  startColour = color(36, 4, 125)
+  endColour = color(217, 20, 49)
 }
 
 function draw() {
-  background(220);
+  background(255);
 
   for (let j = 0; j < DIMENSION; j++) {
     for(let i = 0; i < DIMENSION; i++) {
       const centerX = tileWidth * i + tileWidth / 2
       const centerY = tileWidth * j + tileWidth / 2
-      drawTile(centerX, centerY, tileWidth)
+      drawTile(centerX, centerY, j, tileWidth)
     }
   }
+
+  noLoop()
 }
