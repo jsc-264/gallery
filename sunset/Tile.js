0d9c2e5e602eb801072988a9122e88e4b8be55da
jsc-264 <joestephen264@gmail.com>
2026-04-15 23:12:07 +0100

    sunset: created render method of Tile object

--- sunset/Tile.js
+++ sunset/Tile.js
@@ -2,17 +2,36 @@ class Tile {
     constructor(x, y, j, w) {
         this.pos = createVector(x, y)
         this.w = w
+        this.j = j
 
-        this.circlesRange = map(j, 0, DIMENSION, tileWidth / 3, tileWidth / 10)
-        this.numCircles = map(j, 0, DIMENSION, 10, 2)
+        const circlesRange = map(j, 0, DIMENSION, tileWidth / 3, tileWidth / 10)
+        const numCircles = map(j, 0, DIMENSION, 10, 1)
         this.extraCirclesCoords = []
-        for (let i = 0; i < this.numCircles; i++) {
+        for (let i = 0; i < numCircles; i++) {
             this.extraCirclesCoords.push(
                 createVector(
-                    random(-this.circlesRange, this.circlesRange),
-                    random(-this.circlesRange, this.circlesRange)
+                    random(-circlesRange, circlesRange),
+                    random(-circlesRange, circlesRange)
                 )
             )
         }
+    }
+
+    render() {
+        push()
+        translate(this.pos.x, this.pos.y)
+
+        
+
+        const strokeColT = map(this.j, 0, DIMENSION, 0, 1)
+        const strokeCol = lerpColor(startColour, endColour, strokeColT)
+        stroke(strokeCol)
+        noFill()
+
+        circle(0, 0, this.w)
+        for (let circlePosition of this.extraCirclesCoords){
+            circle(circlePosition.x, circlePosition.y, this.w)
+        }
+        pop()
     }
 }
--- sunset/sketch.js
+++ sunset/sketch.js
@@ -1,34 +1,8 @@
 const DIMENSION = 10
 let tileWidth
+let tiles = []
 
 let startColour, endColour
-
-function drawTile(tileX, tileY, tileJ, tileWidth){
-  push()
-  translate(tileX, tileY)
-
-  noFill()
-
-  const circleWeight = map(tileJ, 0, DIMENSION, 2, 1)
-  strokeWeight(circleWeight)
-
-  const colT = map(tileJ, 0, DIMENSION, 0, 1)
-  const col = lerpColor(startColour, endColour, colT)
-  stroke(col)
-
-  const range = map(tileJ, 0, DIMENSION, tileWidth/3, tileWidth/10)
-
-  const numCircles = map(tileJ, 0, DIMENSION, 10, 0)
-  for (let i = 0; i < numCircles; i++){
-    const circleX = random(-range, range)
-    const circleY = random(-range, range)
-    circle(circleX, circleY, tileWidth)
-  }
-
-
-  circle(0, 0, tileWidth)
-  pop()
-}
 
 function setup() {
   createCanvas(400, 400);
@@ -36,18 +10,20 @@ function setup() {
 
   startColour = color(36, 4, 125)
   endColour = color(217, 20, 49)
+
+  for (let j = 0; j < DIMENSION; j++) {
+    for (let i = 0; i < DIMENSION; i++) {
+      const centerX = tileWidth * i + tileWidth / 2
+      const centerY = tileWidth * j + tileWidth / 2
+      tiles.push(new Tile(centerX, centerY, j, tileWidth))
+    }
+  }
 }
 
 function draw() {
   background(255);
 
-  for (let j = 0; j < DIMENSION; j++) {
-    for(let i = 0; i < DIMENSION; i++) {
-      const centerX = tileWidth * i + tileWidth / 2
-      const centerY = tileWidth * j + tileWidth / 2
-      drawTile(centerX, centerY, j, tileWidth)
-    }
-  }
-
-  noLoop()
+  tiles.forEach(tile => {
+    tile.render()
+  })
 }
