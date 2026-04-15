03f3f539534e6c45ed94083b31d56dbee2220711
jsc-264 <joestephen264@gmail.com>
2026-04-15 23:05:00 +0100

    sunset: created constructor of Tile object

--- sunset/Tile.js
+++ sunset/Tile.js
@@ -1,11 +1,18 @@
 class Tile {
-    constructor(x, y, i, j, w){
+    constructor(x, y, j, w) {
         this.pos = createVector(x, y)
-        this.index = {
-            i: i,
-            j: j
-        },
-
         this.w = w
+
+        this.circlesRange = map(j, 0, DIMENSION, tileWidth / 3, tileWidth / 10)
+        this.numCircles = map(j, 0, DIMENSION, 10, 2)
+        this.extraCirclesCoords = []
+        for (let i = 0; i < this.numCircles; i++) {
+            this.extraCirclesCoords.push(
+                createVector(
+                    random(-this.circlesRange, this.circlesRange),
+                    random(-this.circlesRange, this.circlesRange)
+                )
+            )
+        }
     }
 }
