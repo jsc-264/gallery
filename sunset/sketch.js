b473d943ae97862c87a487da46d983cff6203fca
jsc-264 <joestephen264@gmail.com>
2026-04-15 22:10:55 +0100

    sunset: created grid

--- sunset/sketch.js
+++ sunset/sketch.js
@@ -1,7 +1,17 @@
+const DIMENSION = 10
+let tileWidth
+
 function setup() {
   createCanvas(400, 400);
+  tileWidth = width / DIMENSION
 }
 
 function draw() {
   background(220);
+
+  for (let j = 0; j < DIMENSION; j++) {
+    for(let i = 0; i < DIMENSION; i++) {
+      rect(tileWidth * i, tileWidth * j, tileWidth)
+    }
+  }
 }
