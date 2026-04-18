9439de93acd5246ecf2a39625b261a78fbb17045
jsc-264 <joestephen264@gmail.com>
2026-04-18 16:10:01 +0100

    lines: connected input / output numbers with line

--- lines/sketch.js
+++ lines/sketch.js
@@ -1,14 +1,32 @@
-const DIMENSION = 10
+const DIMENSION = 30
 let nums = []
+
+function formula(num){
+  let val = (num + 1) % DIMENSION**2
+  return val
+}
+
+function findObject(targetValue){
+  for (let obj of nums) {
+    if (obj.value == targetValue){
+      return obj
+    }
+  }
+}
 
 function setup() {
   createCanvas(400, 400);
   textAlign(CENTER, CENTER)
+  const tileWidth = width/DIMENSION
 
-  let num = 1
+  let num = 0
   for (let j = 0; j < DIMENSION; j++) {
     for (let i = 0; i < DIMENSION; i++) {
-      nums.push(num)
+      nums.push({
+        value: num,
+        x: tileWidth * i + tileWidth / 2,
+        y: tileWidth * j + tileWidth / 2
+      })
       num++
     }
   }
@@ -20,7 +38,10 @@ function draw() {
   for (let j = 0; j < DIMENSION; j++) {
     for (let i = 0; i < DIMENSION; i++) {
       const index = j * DIMENSION + i
-      text(nums[index], width / DIMENSION * i + width / DIMENSION / 2, height / DIMENSION * j + width / DIMENSION / 2)
+      const num = nums[index]
+      const result = formula(num.value)
+      const target = findObject(result)
+      line(num.x, num.y, target.x, target.y)
     }
   }
 }
