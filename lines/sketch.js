4707f3fadeff7c78ed541e90fd23d385d6dbc3de
jsc-264 <joestephen264@gmail.com>
2026-04-18 15:53:18 +0100

    lines: created number grid from 1-100

--- lines/sketch.js
+++ lines/sketch.js
@@ -1,7 +1,26 @@
+const DIMENSION = 10
+let nums = []
+
 function setup() {
   createCanvas(400, 400);
+  textAlign(CENTER, CENTER)
+
+  let num = 1
+  for (let j = 0; j < DIMENSION; j++) {
+    for (let i = 0; i < DIMENSION; i++) {
+      nums.push(num)
+      num++
+    }
+  }
 }
 
 function draw() {
   background(220);
+
+  for (let j = 0; j < DIMENSION; j++) {
+    for (let i = 0; i < DIMENSION; i++) {
+      const index = j * DIMENSION + i
+      text(nums[index], width / DIMENSION * i + width / DIMENSION / 2, height / DIMENSION * j + width / DIMENSION / 2)
+    }
+  }
 }
