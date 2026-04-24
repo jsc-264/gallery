b297494bfa1c731b46710cbe8f2d7995f6e4d882
jsc-264 <joestephen264@gmail.com>
2026-04-24 23:04:00 +0100

    forest: cahnged colours, shape and dimensions

--- forest/sketch.js
+++ forest/sketch.js
@@ -1,10 +1,11 @@
-const DIMENSION = 30
+const DIMENSION = 10
 const colours = []
 let nz = 0
 
 function setup() {
   createCanvas(400, 400);
   colorMode(HSL)
+
   noStroke()
   background(0, 0, 90);
 
@@ -14,7 +15,7 @@ function setup() {
 }
 
 function draw() {
-  for (let a = 0; a < 10; a++) {
+  for (let a = 0; a < DIMENSION; a++) {
     for (let j = 0; j < DIMENSION; j++) {
       for (let i = 0; i < DIMENSION; i++) {
         const n = noise(i / 10, j / 10, nz)
@@ -30,12 +31,12 @@ function draw() {
 
     const n = colours[i][j]
     const col = color(
-      map(n, 0, 1, 35, 50),
+      map(n, 0, 1, 0, 360),
       map(n, 0, 1, 0, 100),
-      map(n, 0, 1, 10, 50),
+      map(n, 0, 1, 0, 100),
     )
     fill(col)
-    rect(x, y, width / DIMENSION, height / DIMENSION)
+    ellipse(x, y, width / DIMENSION, height / DIMENSION)
 
     nz += 0.0005
   }
