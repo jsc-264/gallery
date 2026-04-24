37b677f437a5f7d66dc4f5abd30c335c37408e87
jsc-264 <joestephen264@gmail.com>
2026-04-24 15:23:18 +0100

    forest: drew singular tree trunck

--- forest/sketch.js
+++ forest/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
   createCanvas(400, 400);
+  colorMode(HSL)
+  noStroke()
 }
 
 function draw() {
-  background(220);
+  background(0, 0, 90);
+  
+  fill(40, 59, 29)
+  rect(50, 50, 20, 50)
 }
