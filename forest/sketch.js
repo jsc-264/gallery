64bf32928bd7e9efac81c599ead4f8eed487b5a2
jsc-264 <joestephen264@gmail.com>
2026-04-24 15:34:18 +0100

    forest: created perlin grid

--- forest/sketch.js
+++ forest/sketch.js
@@ -1,12 +1,17 @@
+const DIMENSION = 20
+
 function setup() {
   createCanvas(400, 400);
   colorMode(HSL)
   noStroke()
+  background(0, 0, 90);
 }
 
 function draw() {
-  background(0, 0, 90);
-  
-  fill(40, 59, 29)
-  rect(50, 50, 20, 50)
+  const x = floor(random(DIMENSION))
+  const y = floor(random(DIMENSION))
+
+  const n = noise(x/10, y/10)
+  fill(n*100)
+  rect(width/DIMENSION*x, height/DIMENSION*y, width/DIMENSION, height/DIMENSION)
 }
