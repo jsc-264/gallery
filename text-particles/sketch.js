f2ce1e5c3292341c8ef72375e06ea5af8073d6b5
jsc-264 <joestephen264@gmail.com>
2026-05-07 20:11:03 +0100

    text-particles: drew message in font

--- text-particles/sketch.js
+++ text-particles/sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-    createCanvas(800, 600);
+const msg = "I Love You"
+
+const url = "https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap"
+let font
+
+async function setup() {
+    createCanvas(windowWidth*0.9, windowHeight*0.9);
+    font = await loadFont(url)
+    textFont(font)
+    textSize(50)
+    textAlign(CENTER, CENTER)
 }
 
 function draw() {
     background(220);
+    push()
+    translate(width/2, height/2)
+    text(msg, 0, 0)
+    pop()
 }
