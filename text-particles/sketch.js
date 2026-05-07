7342690032e7963350bf6e0186172337ccfe7efb
jsc-264 <joestephen264@gmail.com>
2026-05-07 20:23:13 +0100

    text-particles: drew message with points

--- text-particles/sketch.js
+++ text-particles/sketch.js
@@ -1,20 +1,36 @@
 const msg = "I Love You"
 
-const url = "https://fonts.googleapis.com/css2?family=Darumadrop+One&display=swap"
+const url = "./assets/DarumadropOne-Regular.ttf"
 let font
 
 async function setup() {
-    createCanvas(windowWidth*0.9, windowHeight*0.9);
+    createCanvas(windowWidth*0.99, windowHeight*0.99);
     font = await loadFont(url)
     textFont(font)
-    textSize(50)
+    textSize(70)
     textAlign(CENTER, CENTER)
+
+    noFill()
+    stroke(0)
+    strokeWeight(2)
 }
 
 function draw() {
     background(220);
     push()
     translate(width/2, height/2)
-    text(msg, 0, 0)
+    // text(msg, 0, 0)
+
+    const options = {
+        sampleFactor: 1,
+        simplifyThreshold: 0
+    }
+    const messagePoints = font.textToContours(msg, 0, 0, options)
+
+    for (let letterPoints of messagePoints){
+        for (let p of letterPoints){
+            point(p.x, p.y)
+        }
+    }
     pop()
 }
