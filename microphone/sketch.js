71419a30a780436af78fc0c1a4a0bb60c4d5cb0f
jsc-264 <joestephen264@gmail.com>
2026-05-10 17:25:28 +0100

    microphone: initialised microphone

--- microphone/sketch.js
+++ microphone/sketch.js
@@ -1,7 +1,17 @@
+let mic
+
 function setup() {
-  createCanvas(400, 400);
+  getAudioContext().suspend();
+  createCanvas(windowWidth, windowHeight);
+
+  mic = new p5.AudioIn()
+  mic.start()
 }
 
 function draw() {
   background(220);
+}
+
+function mousePressed(){
+  userStartAudio()
 }
