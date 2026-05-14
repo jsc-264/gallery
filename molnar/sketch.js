2ea8e7617370f261711a6a5465b44007854777bc
jsc-264 <joestephen264@gmail.com>
2026-05-14 20:04:11 +0100

    molnar: created grid of squares

--- molnar/sketch.js
+++ molnar/sketch.js
@@ -1,7 +1,19 @@
+const DIM = 10
+let s
+
 function setup() {
     createCanvas(500, 500);
+    s = width/DIM
 }
 
 function draw() {
     background(50);
+
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            const x = s * i
+            const y = s * j
+            square(x, y, s)
+        }
+    }
 }
