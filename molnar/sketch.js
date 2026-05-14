f2ed4a158e16350d6f9421bc9836862ddcc2a915
jsc-264 <joestephen264@gmail.com>
2026-05-14 20:18:06 +0100

    molnar: added colour and centered grid

--- molnar/sketch.js
+++ molnar/sketch.js
@@ -1,23 +1,35 @@
 const DIM = 10
 let s
 
+let colours
+
 function shape(x, y, s) {
-    const numShapes = random(4, 7)
-    stroke(255)
+    const numShapes = random(1, 7)
+    const wiggle = 10
+    stroke(random(colours))
     noFill()
     for (let i = 0; i < numShapes; i++) {
         beginShape()
-        vertex(x     + random(-5, 5), y     + random(-5, 5))
-        vertex(x + s + random(-5, 5), y     + random(-5, 5))
-        vertex(x + s + random(-5, 5), y + s + random(-5, 5))
-        vertex(x     + random(-5, 5), y + s + random(-5, 5))
-        endShape()
+        vertex(x     + random(-wiggle, wiggle), y     + random(-wiggle, wiggle))
+        vertex(x + s + random(-wiggle, wiggle), y     + random(-wiggle, wiggle))
+        vertex(x + s + random(-wiggle, wiggle), y + s + random(-wiggle, wiggle))
+        vertex(x     + random(-wiggle, wiggle), y + s + random(-wiggle, wiggle))
+        endShape(CLOSE)
     }
 }
 
 function setup() {
     createCanvas(500, 500);
-    s = width / DIM
+    s = width / DIM*0.9
+
+    colours = [
+        // red, green, cyan, yellow, pink
+        color(255, 0, 0),
+        color(0, 255, 0),
+        color(0, 255, 255),
+        color(255, 255, 0),
+        color(255, 0, 255)
+    ]
 }
 
 function draw() {
@@ -25,9 +37,11 @@ function draw() {
 
     for (let j = 0; j < DIM; j++) {
         for (let i = 0; i < DIM; i++) {
-            const x = s * i
-            const y = s * j
+            const x = (s * i) + s/2
+            const y = (s * j) + s/2
             shape(x, y, s)
         }
     }
+
+    noLoop()
 }
