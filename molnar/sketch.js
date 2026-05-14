141d3d83c8550420b339ece2d3cfff91bc846092
jsc-264 <joestephen264@gmail.com>
2026-05-14 20:22:04 +0100

    molnar: cahnged bg of html to fit canvas

--- molnar/sketch.js
+++ molnar/sketch.js
@@ -10,17 +10,18 @@ function shape(x, y, s) {
     noFill()
     for (let i = 0; i < numShapes; i++) {
         beginShape()
-        vertex(x     + random(-wiggle, wiggle), y     + random(-wiggle, wiggle))
-        vertex(x + s + random(-wiggle, wiggle), y     + random(-wiggle, wiggle))
-        vertex(x + s + random(-wiggle, wiggle), y + s + random(-wiggle, wiggle))
-        vertex(x     + random(-wiggle, wiggle), y + s + random(-wiggle, wiggle))
+        vertex(x + random(-wiggle, wiggle), y + random(-wiggle, wiggle))
+        vertex(x + sx + random(-wiggle, wiggle), y + random(-wiggle, wiggle))
+        vertex(x + sx + random(-wiggle, wiggle), y + sy + random(-wiggle, wiggle))
+        vertex(x + random(-wiggle, wiggle), y + sy + random(-wiggle, wiggle))
         endShape(CLOSE)
     }
 }
 
 function setup() {
-    createCanvas(500, 500);
-    s = width / DIM*0.9
+    createCanvas(windowWidth * 0.9, windowHeight * 0.9);
+    sx = width / DIM * 0.9
+    sy = height / DIM * 0.9
 
     colours = [
         // red, green, cyan, yellow, pink
@@ -33,15 +34,21 @@ function setup() {
 }
 
 function draw() {
-    background(50);
+    background(30);
 
     for (let j = 0; j < DIM; j++) {
         for (let i = 0; i < DIM; i++) {
-            const x = (s * i) + s/2
-            const y = (s * j) + s/2
+            const x = (sx * i) + sx / 2
+            const y = (sy * j) + sy / 2
             shape(x, y, s)
         }
     }
 
     noLoop()
+}
+
+function keyPressed(){
+    if (key == " "){
+        redraw()
+    }
 }
