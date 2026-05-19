a4b1d0b346f2d818a994780ce34ce774e6ce4f8c
jsc-264 <joestephen264@gmail.com>
2026-05-19 14:34:28 +0100

    blake: created square grid

--- blake/sketch.js
+++ blake/sketch.js
@@ -1,9 +1,23 @@
+const DIM = 5
+let tWidth
+
 function setup() {
-    createCanvas(windowWidth * 0.9, windowHeight * 0.9);
+    createCanvas(600, 600);
 }
 
 function draw() {
     background(220);
+
+    tWidth = width / DIM
+
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            const x = tWidth * i
+            const y = tWidth * j
+
+            rect(x, y, tWidth)
+        }
+    }
 }
 
 function windowResized() {
