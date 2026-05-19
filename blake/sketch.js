1828240dd7e6f900e208c4001230b5de965a7e27
jsc-264 <joestephen264@gmail.com>
2026-05-19 14:51:22 +0100

    blake: created concentric circles

--- blake/sketch.js
+++ blake/sketch.js
@@ -1,6 +1,30 @@
 const DIM = 5
 let tWidth
 
+function concentricCircles(x, y, w) {
+    const layers = 3
+    const black = color(0)
+    const white = color(255)
+    let currentFill = random([black, white])
+
+    fill(currentFill)
+    rect(x, y, w, w)
+
+    push()
+    translate(x + w / 2, y + w / 2)
+    for (let i = layers; i > 0; i--){
+        if (currentFill == black){
+            currentFill = white
+        } else {
+            currentFill = black
+        }
+
+        fill(currentFill)
+        circle(0, 0, i*w/layers)
+    }
+    pop()
+}
+
 function setup() {
     createCanvas(600, 600);
 }
@@ -14,10 +38,10 @@ function draw() {
         for (let i = 0; i < DIM; i++) {
             const x = tWidth * i
             const y = tWidth * j
-
-            rect(x, y, tWidth)
+            concentricCircles(x, y, tWidth)
         }
     }
+    noLoop()
 }
 
 function windowResized() {
