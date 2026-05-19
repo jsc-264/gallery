18d3782dafea4e0e9bc4d1f50d55752d0058a437
jsc-264 <joestephen264@gmail.com>
2026-05-19 14:23:12 +0100

    blake: added resizecanvas and formatting

--- blake/sketch.js
+++ blake/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(windowWidth * 0.9, windowHeight * 0.9);
 }
 
 function draw() {
     background(220);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth * 0.9, windowHeight * 0.9)
 }
