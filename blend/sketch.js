8e9b5ef822c7ee025a1a1d56c6327ed12aaf0ea0
jsc-264 <joestephen264@gmail.com>
2026-06-05 13:40:55 +0100

    blend: drawn random rectangles

--- blend/sketch.js
+++ blend/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
     createCanvas(800, 600);
+    rectMode(CENTER)
 }
 
 function draw() {
     background(220);
+
+    for (let i = 0; i < 10; i++){
+        rect(
+            random(width),
+            random(height),
+            random(200, width/2),
+            random(200, height/2)
+        )
+    }
+
+    noLoop()
 }
