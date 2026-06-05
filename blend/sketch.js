f8cb0361b2c46c56e1e35744768716be3d97eeed
jsc-264 <joestephen264@gmail.com>
2026-06-05 13:50:30 +0100

    blend: added colour and blur to rectangles

--- blend/sketch.js
+++ blend/sketch.js
@@ -1,19 +1,31 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(windowWidth, windowHeight);
     rectMode(CENTER)
 }
 
 function draw() {
-    background(220);
+    push()
 
     for (let i = 0; i < 10; i++){
+        push()
+        fill(
+            random(255),
+            random(255),
+            random(255)
+        )
+        noStroke()
         rect(
             random(width),
             random(height),
             random(200, width/2),
             random(200, height/2)
         )
+        filter("blur", random(1, 5))
+        pop()
     }
+
+    filter("blur", random(5))
+    pop()
 
     noLoop()
 }
