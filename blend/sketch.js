ce4b58aebc0b18435d626082161a94c5bdd8007a
jsc-264 <joestephen264@gmail.com>
2026-06-05 16:39:07 +0100

    blend: converted to p5 1.x project

--- blend/sketch.js
+++ blend/sketch.js
@@ -1,52 +1,59 @@
 function static(intensity = 20) {
-    loadPixels()
+  loadPixels()
 
-    for (let i = 0; i < pixels.length; i++) {
-        pixels[i] += random(-intensity, intensity)
-    }
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i] += random(-intensity, intensity)
+  }
 
-    updatePixels()
+  updatePixels()
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER)
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER)
 }
 
 function draw() {
-    background(200)
+  background(200)
 
-    for (let i = 0; i < 10; i++) {
-        push()
-        fill(
-            random(255),
-            random(255),
-            random(255)
-        )
-        noStroke()
+  const start = millis()
 
-        if (random() < 0.5) {
-            rect(
-                random(width),
-                random(height),
-                random(200, width),
-                random(200, height)
-            )
-        } else {
-            ellipse(
-                random(width),
-                random(height),
-                random(200, width / 2),
-                random(200, height / 2)
-            )
-        }
-        filter("blur", random(10, 30))
-        pop()
+  for (let i = 0; i < 10; i++) {
+    push()
+    fill(
+      random(255),
+      random(255),
+      random(255)
+    )
+    noStroke()
+
+    if (random() < 0.5) {
+      rect(
+        random(width),
+        random(height),
+        random(200, width),
+        random(200, height)
+      )
+    } else {
+      ellipse(
+        random(width),
+        random(height),
+        random(200, width / 2),
+        random(200, height / 2)
+      )
     }
+    drawingContext.filter = "blur(8px)"
+    pop()
+  }
 
-    static(intensity = random(10, 50))
+  static(intensity = random(10, 50))
+
+  const end = millis()
+
+  print(end - start)
+  noLoop()
 }
 
-function windowResized(){
-    resizeCanvas(windowWidth, windowHeight)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
