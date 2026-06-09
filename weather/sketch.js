4f839f26f63d2c07ba19a9569aa0ae515324c8c4
jsc-264 <joestephen264@gmail.com>
2026-06-09 15:20:44 +0100

    weather: made grid

--- weather/sketch.js
+++ weather/sketch.js
@@ -1,7 +1,23 @@
+const DIM = 20
+let w
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(600, 600);
+    colorMode(HSB, 360, 255, 255)
+    w = width / DIM
 }
 
 function draw() {
     background(220);
+
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            const x = i * w
+            const y = j * w
+            
+            rect(x, y, w)
+        }
+    }
+
+    noLoop()
 }
