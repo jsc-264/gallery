1114a32eba609db166920fce123dc754746f03f9
jsc-264 <joestephen264@gmail.com>
2026-06-09 15:28:48 +0100

    weather: filled squares based off temp

--- weather/sketch.js
+++ weather/sketch.js
@@ -1,20 +1,31 @@
 const DIM = 20
 let w
 
+let hot, cold
+
 function setup() {
     createCanvas(600, 600);
-    colorMode(HSB, 360, 255, 255)
     w = width / DIM
+
+    hot = color(255, 0, 0)
+    cold = color(0, 0, 255)
 }
 
 function draw() {
     background(220);
 
     for (let j = 0; j < DIM; j++) {
+        const long = map(j, 0, DIM, -180, 180)
         for (let i = 0; i < DIM; i++) {
             const x = i * w
             const y = j * w
-            
+
+            const lat = map(i, 0, DIM, -90, 90)
+            const temp = random(0, 30)
+
+            const amt = map(temp, 0, 30, 0, 1)
+            const col = lerpColor(cold, hot, amt)
+            fill(col)
             rect(x, y, w)
         }
     }
