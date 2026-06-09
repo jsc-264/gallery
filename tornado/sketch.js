63332ca72b2fc53d362d1745f86dee3b871d5d7a
jsc-264 <joestephen264@gmail.com>
2026-06-09 16:39:33 +0100

    tornado: created singular spinning body

--- tornado/sketch.js
+++ tornado/sketch.js
@@ -1,7 +1,18 @@
+let r = 100
+let theta = 0
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(600, 600);
 }
 
 function draw() {
-    background(220);
+    background(50);
+    translate(width / 2, height / 2)
+
+    const x = r * cos(theta)
+    const y = r * sin(theta)
+
+    circle(x, y, 20)
+
+    theta += 0.1
 }
