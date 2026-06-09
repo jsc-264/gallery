338912f1f6788fe3d2f53e68123f6bdaee4b93b7
jsc-264 <joestephen264@gmail.com>
2026-06-09 15:56:48 +0100

    weather: get rid of grid and repeated api calls

--- weather/sketch.js
+++ weather/sketch.js
@@ -1,34 +1,16 @@
-const DIM = 20
+const DIM = 10
 let w
 
-let hot, cold
+let hotCol, coldCol
 
-function setup() {
+async function setup() {
     createCanvas(600, 600);
     w = width / DIM
 
-    hot = color(255, 0, 0)
-    cold = color(0, 0, 255)
+    hotCol = color(255, 0, 0)
+    coldCol = color(0, 0, 255)
 }
 
 function draw() {
     background(220);
-
-    for (let j = 0; j < DIM; j++) {
-        const long = map(j, 0, DIM, -180, 180)
-        for (let i = 0; i < DIM; i++) {
-            const x = i * w
-            const y = j * w
-
-            const lat = map(i, 0, DIM, -90, 90)
-            const temp = random(0, 30)
-
-            const amt = map(temp, 0, 30, 0, 1)
-            const col = lerpColor(cold, hot, amt)
-            fill(col)
-            rect(x, y, w)
-        }
-    }
-
-    noLoop()
 }
