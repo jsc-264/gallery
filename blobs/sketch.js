732f7802c24484a922f67fd3f4a8f1a71e8d25b9
jsc-264 <joestephen264@gmail.com>
2026-07-19 20:05:38 +0100

    blobs: drew points of circle

--- blobs/sketch.js
+++ blobs/sketch.js
@@ -1,7 +1,23 @@
+let rad = 100
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(500, 500);
+    angleMode(DEGREES)
 }
 
 function draw() {
     background(220);
+    push()
+
+    translate(width/2, height/2)
+    beginShape()
+    for (let a = 0; a < 360; a++){
+        const x = rad * cos(a)
+        const y = rad * sin(a)
+
+        vertex(x, y)
+    }
+    endShape(CLOSE)
+
+    pop()
 }
