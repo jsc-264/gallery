dfbabb198ff1f04d0ae8dfabab55b9780926e7f8
jsc-264 <joestephen264@gmail.com>
2026-07-19 20:31:44 +0100

    blob: adjusted points of circle to be iregular

--- blobs/sketch.js
+++ blobs/sketch.js
@@ -2,22 +2,25 @@ let rad = 100
 
 function setup() {
     createCanvas(500, 500);
-    angleMode(DEGREES)
+    angleMode(DEGREES);
+    noFill()
 }
 
 function draw() {
-    background(220);
-    push()
-
-    translate(width/2, height/2)
     beginShape()
     for (let a = 0; a < 360; a++){
-        const x = rad * cos(a)
-        const y = rad * sin(a)
+        const nx = (rad * cos(a) + width/2)/50
+        const ny = (rad * sin(a) + height/2)/50
+        const nVal = noise(nx, ny)
+
+        const change = map(nVal, 0, 1, -20, 20)
+        const pRad = rad + change
+
+        const x = pRad * cos(a) + width / 2
+        const y = pRad * sin(a) + height / 2
 
         vertex(x, y)
     }
     endShape(CLOSE)
-
-    pop()
+    noLoop()
 }
