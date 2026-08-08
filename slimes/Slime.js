84e8d2fc06cac3ce3b4ebfb2836232be082b0ddf
jsc-264 <joestephen264@gmail.com>
2026-08-08 18:29:16 +0100

    slimes: added colour and border rad

--- slimes/Slime.js
+++ slimes/Slime.js
@@ -6,17 +6,24 @@ class Slime {
 
         this.vx = random(2, 5) * random([-1, 1])
         this.vy = random(2, 5) * random([-1, 1])
+
+        const hu = random(360)
+        this.fill = color(hu, 50, 100)
+        this.border = color(hu, 100, 100)
     }
 
     render(){
-        rect(this.x, this.y, this.w, this.h)
+        strokeWeight(5)
+        stroke(this.border)
+        fill(this.fill)
+        rect(this.x, this.y, this.w, this.h, 20)
     }
 
     grow(){
         let pidX = map(this.x, 0, width, 0, PI)
         let pidY = map(this.y, 0, height, 0, PI)
-        let newW = map(sin(pidX), 0, 1, 0, 100)
-        let newH = map(sin(pidY), 0, 1, 0, 100)
+        let newW = map(sin(pidX), 0, 1, 0, width/ 4)
+        let newH = map(sin(pidY), 0, 1, 0, height/4)
 
         this.w = newW
         this.h = newH
--- slimes/sketch.js
+++ slimes/sketch.js
@@ -4,13 +4,14 @@ let numSlimes = 20
 function setup() {
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER)
+    colorMode(HSB)
     for (let i = 0; i < numSlimes; i++) {
         slimes.push(new Slime())
     }
 }
 
 function draw() {
-    background(100);
+    background(0, 0, 25);
     for (let s of slimes) {
         s.update()
         s.render()
