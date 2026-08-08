b12e1bbadbb4fbf3486634938add1c216c0b4efb
jsc-264 <joestephen264@gmail.com>
2026-08-08 18:06:42 +0100

    slimes: made canvas fullscreen

--- slimes/sketch.js
+++ slimes/sketch.js
@@ -2,7 +2,7 @@ let slimes = []
 let numSlimes = 20
 
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(windowWidth, windowHeight);
     rectMode(CENTER)
     for (let i = 0; i < numSlimes; i++) {
         slimes.push(new Slime())
