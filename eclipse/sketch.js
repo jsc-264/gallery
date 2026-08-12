6655fe284d5f54a2854d964cfc826f98534da29a
jsc-264 <joestephen264@gmail.com>
2026-08-12 20:43:33 +0100

    eclipse: chnaged colour of bg based of dist of bodies

--- eclipse/sketch.js
+++ eclipse/sketch.js
@@ -1,13 +1,26 @@
 let sun, moon
 
+let brightCol, darkCol
+
+function BodyDistance(body1, body2) {
+    return dist(body1.pos.x, body1.pos.y, body2.pos.x, body2.pos.y)
+}
+
 function setup() {
     createCanvas(800, 600);
+    brightCol = color(163, 196, 255)
+    darkCol = color(5, 0, 37)
+
     sun = new Body(width / 2, height / 2, false)
     moon = new Body(100, 100, true)
 }
 
 function draw() {
-    background(220);
+    const d = BodyDistance(sun, moon)
+    const darkness = map(d, 0, 500, 1, 0)
+    print(d, darkness)
+    const col = lerpColor(brightCol, darkCol, darkness)
+    background(col);
     sun.render()
 
     moon.update()
@@ -20,7 +33,7 @@ class Body {
         this.moving = moving
 
         if (this.moving) {
-            this.vel = createVector(5, 0)
+            this.vel = createVector(10, 0)
             this.angle = 0.05
             this.vel.rotate(this.angle)
         }
@@ -37,6 +50,10 @@ class Body {
             if (this.pos.x > width + this.r) {
                 this.pos.x = -this.r
                 this.pos.y += this.r / 4
+            }
+
+            if (this.pos.y > height + this.r){
+                this.pos.y = -this.r
             }
             this.pos.add(this.vel)
         }
