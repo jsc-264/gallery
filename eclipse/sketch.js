ed26e4d031a8d7172256bb03729ab8a9ded9598d
jsc-264 <joestephen264@gmail.com>
2026-08-12 20:31:35 +0100

    eclipse: created moonobject instance

--- eclipse/sketch.js
+++ eclipse/sketch.js
@@ -1,26 +1,44 @@
-let sun
+let sun, moon
 
 function setup() {
     createCanvas(800, 600);
-    sun = new Body(width/2, height/2)
+    sun = new Body(width / 2, height / 2, false)
+    moon = new Body(100, 100, true)
 }
 
 function draw() {
     background(220);
     sun.render()
+
+    moon.update()
+    moon.render()
 }
 
-class Body{
-    constructor(x, y){
+class Body {
+    constructor(x, y, moving) {
         this.pos = createVector(x, y)
-        this.vel = createVector(1, 0)
-        this.angle = 0.1
-        this.vel.rotate(this.angle)
+        this.moving = moving
+
+        if (this.moving) {
+            this.vel = createVector(5, 0)
+            this.angle = 0.05
+            this.vel.rotate(this.angle)
+        }
 
         this.r = 100
     }
 
-    render(){
+    render() {
         circle(this.pos.x, this.pos.y, this.r)
+    }
+
+    update() {
+        if (this.moving) {
+            if (this.pos.x > width + this.r) {
+                this.pos.x = -this.r
+                this.pos.y += this.r / 4
+            }
+            this.pos.add(this.vel)
+        }
     }
 }
