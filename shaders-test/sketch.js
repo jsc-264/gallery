60d0c71ab8bb7808ff1679106e747de75f11cf30
jsc-264 <joestephen264@gmail.com>
2026-08-18 21:30:08 +0100

    shader-test: added boilerplate for shaders

--- shaders-test/sketch.js
+++ shaders-test/sketch.js
@@ -1,7 +1,13 @@
-function setup() {
-    createCanvas(800, 600);
+let shad
+
+async function setup() {
+    createCanvas(800, 600, WEBGL);
+    shad = await loadShader("shader.vert", "shader.frag")
+    shader(shad)
+    noStroke()
 }
 
 function draw() {
-    background(220);
+    clear()
+    rect(0, 0, width, height)
 }
