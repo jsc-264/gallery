354e4b2baf1b828f4971aef40cff60124f38ae2f
jsc-264 <joestephen264@gmail.com>
2026-08-26 16:13:46 +0100

    tartan: created colour palette

--- tartan/sketch.js
+++ tartan/sketch.js
@@ -1,7 +1,18 @@
+let palette
+
 function setup() {
     createCanvas(800, 600);
+    palette = {
+        bg: color("#F5DEB3"),
+        lines: [
+            color("#000080"),
+            color("#006400"),
+            color("#B22222"),
+            color("#DAA520"),
+        ]
+    }
 }
 
 function draw() {
-    background(220);
+    background(palette.bg);
 }
