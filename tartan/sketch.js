dc36ed3c2dc9d5dc0f1e770ee528cf0fdf3f4225
jsc-264 <joestephen264@gmail.com>
2026-08-26 17:10:56 +0100

    tartan: started over

--- tartan/sketch.js
+++ tartan/sketch.js
@@ -4,6 +4,12 @@ let maxVertLines, maxHorzLines
 let currentVertLines = []
 let currentHorzLines = []
 
+const maxSize = 100
+
+function touching(x1, w1, x2, w2) {
+    return x1 > x2 - w1 && x1 < x2 + w2
+}
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -16,34 +22,11 @@ function setup() {
             color("#DAA520"),
         ]
     }
-
-    maxVertLines = width / 100
-    maxVorzLines = height / 100
-
     noStroke()
 }
 
 function draw() {
     background(palette.bg);
-
-    while (currentVertLines.length < maxVertLines) {
-        const lineWidth = random(10, width / 4)
-        const lineX = random(0, width - w)
-        const col = random(palette.lines)
-
-        let crossing = false
-        for (let i = 0; i < currentVertLines.length; i++) {
-            const lineData = currentVertLines[i]
-            if (!(lineX < lineData.x - lineWidth && lineX > lineData.x + lineData.w)){
-                crossing = true
-            }
-        }
-
-        if (!crossing){
-            currentVertLines.push({x: lineX, w: lineWidth, col: col})
-        }
-    }
-
 
     noLoop()
 }
