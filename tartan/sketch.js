8e488c63abaa73f4f848d52c2accb7edc875e6b7
jsc-264 <joestephen264@gmail.com>
2026-08-26 17:15:25 +0100

    tartan: created singluar line

--- tartan/sketch.js
+++ tartan/sketch.js
@@ -1,15 +1,14 @@
 let palette
-let maxVertLines, maxHorzLines
+let maxLines = 100
 
-let currentVertLines = []
-let currentHorzLines = []
+let lines = []
 
-const maxSize = 100
-
-function touching(x1, w1, x2, w2) {
-    return x1 > x2 - w1 && x1 < x2 + w2
+function drawLines() {
+    for (let line of lines) {
+        fill(line.col)
+        rect(line.x, line.y, line.w, line.h)
+    }
 }
-
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -28,6 +27,17 @@ function setup() {
 function draw() {
     background(palette.bg);
 
+    lines.push({
+        col: random(palette.lines),
+        x: width/2,
+        y: 0,
+        w: 10,
+        h: height
+    })
+
+    if (lines.length > 0) {
+        drawLines()
+    }
     noLoop()
 }
 
