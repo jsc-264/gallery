f204ffafe8a68f4e5d812ce5deddba8a9eceefd1
jsc-264 <joestephen264@gmail.com>
2026-08-29 19:11:00 +0100

    tapestry: made grid offset

--- tapestry/sketch.js
+++ tapestry/sketch.js
@@ -1,22 +1,23 @@
 const DIM = 10
 let w
+let coords = []
 
 function setup() {
     createCanvas(500, 500);
     w = width / DIM
-}
 
-function draw() {
-    background(220);
-
-    for (let j = 0; j < DIM; j++) {
+    background(220)
+    for (let j = 0; j < DIM * 2; j++) {
         for (let i = 0; i < DIM; i++) {
-            const x = w * i
-            const y = w * j
+            const x = j % 2 == 0 ? w * i : (w * i) + w / 2
+            const y = (w * j) / 2
 
-            stroke(0)
             strokeWeight(5)
             point(x, y)
         }
     }
 }
+
+// function draw() {
+//     background(220);
+// }
