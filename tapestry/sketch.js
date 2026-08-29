6e4ef5c6bb97dbe35f7e5fe3145aed4573d10441
jsc-264 <joestephen264@gmail.com>
2026-08-29 19:07:36 +0100

    tapestry: made grid

--- tapestry/sketch.js
+++ tapestry/sketch.js
@@ -1,7 +1,22 @@
+const DIM = 10
+let w
+
 function setup() {
     createCanvas(500, 500);
+    w = width / DIM
 }
 
 function draw() {
     background(220);
+
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            const x = w * i
+            const y = w * j
+
+            stroke(0)
+            strokeWeight(5)
+            point(x, y)
+        }
+    }
 }
