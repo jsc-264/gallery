d5244935917616f1fb6568d6f2d0e73833c91697
jsc-264 <joestephen264@gmail.com>
2026-08-31 15:05:52 +0100

    waves: made a sine wave

--- waves/sketch.js
+++ waves/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(500, 500);
+    noStroke()
 }
 
 function draw() {
-    background(220);
+    background(0);
+
+    let scl = 100
+    let time = millis()/5
+    for (let i = 0; i < height; i++) {
+        circle(width/2 + sin((i+time)/(scl))*scl, i, 50)
+    }
 }
