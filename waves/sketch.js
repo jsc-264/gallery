aba35d3e5f47c4601c31fdb368ac5efb84025237
jsc-264 <joestephen264@gmail.com>
2026-08-31 15:40:43 +0100

    waves: added colour and multiple waves

--- waves/sketch.js
+++ waves/sketch.js
@@ -1,14 +1,28 @@
+let scl = 40
+let time, off
+let quant = 1.5
+let rows = 3
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(windowWidth, windowHeight);
     noStroke()
 }
 
 function draw() {
     background(0);
 
-    let scl = 60
-    let time = millis() / 5
-    for (let i = 0; i < height; i++) {
-        circle(width / 2 + sin((i + time) / (scl)) * scl + i/2, i, scl + sin((i + time) / scl) * (scl*sin(time/50)))
+    for (let i = 0; i < rows; i++) {
+        off = map(i, 0, rows - 1, 100, width - 100)
+        time = millis() / 5
+        for (let i = -scl; i < (height + scl) / quant; i++) {
+            fill(sin(time / 10) * 10 + abs(sin((i + time) / (scl)) * scl),
+                sin(time / 100) * 100 + abs(sin((i + time) / (scl)) * scl),
+                20 + sin(time / 50) * 200 + abs(sin((i + time) / (scl)) * scl)
+            )
+            circle(
+                off + sin((i + time) / (scl)) * scl,
+                i * quant,
+                scl + sin(time/100)*20 + sin((i + time) / scl) * (scl * sin(time / scl)))
+        }
     }
 }
