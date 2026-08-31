f8f13d18b2224fe5051d4b3ec4c03cbb77a73385
jsc-264 <joestephen264@gmail.com>
2026-08-31 15:18:39 +0100

    waves: created distored wave

--- waves/sketch.js
+++ waves/sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(600, 600);
     noStroke()
 }
 
 function draw() {
     background(0);
 
-    let scl = 100
-    let time = millis()/5
+    let scl = 60
+    let time = millis() / 5
     for (let i = 0; i < height; i++) {
-        circle(width/2 + sin((i+time)/(scl))*scl, i, 50)
+        circle(width / 2 + sin((i + time) / (scl)) * scl + i/2, i, scl + sin((i + time) / scl) * (scl*sin(time/50)))
     }
 }
