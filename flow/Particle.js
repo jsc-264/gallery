0f2df84b5487935d3e6426dd5ebca127ba522312
jsc-264 <joestephen264@gmail.com>
2026-09-05 15:05:37 +0100

    flow: started over

--- flow/Particle.js
+++ flow/Particle.js
@@ -4,6 +4,7 @@ class Particle {
         this.r = r
         this.path = []
         this.vel = createVector(0, 0)
+        this.acc = createVector(0, 0)
     }
 
     render() {
@@ -11,7 +12,10 @@ class Particle {
     }
 
     update() {
+        this.vel.add(this.acc)
+        this.vel.limit(4)
         this.pos.add(this.vel)
+        this.acc.mult(0)
 
         if (this.pos.x < 0) this.pos.x = width
         if (this.pos.x > width) this.pos.x = 0
@@ -19,12 +23,16 @@ class Particle {
         if (this.pos.y > height) this.pos.y = 0
     }
 
+    push(vec){
+        this.acc.add(vec)
+    }
+
     setVel(field) {
         const i = floor((this.pos.x / w) % FIELD_DIM)
         const j = floor((this.pos.y / w) % FIELD_DIM)
         const angle = field[i][j]
-        const vec = p5.Vector.fromAngle(angle).setMag(1)
-        this.vel.add(vec).setMag(2)
+        const vec = p5.Vector.fromAngle(angle).setMag(10)
+        this.push(vec)
     }
 
 }
--- flow/sketch.js
+++ flow/sketch.js
@@ -1,62 +1,13 @@
 const FIELD_DIM = 20
-let angleField
-let w
-
-let particles = []
-let numParticles = 1
-
-function makeAngleField() {
-    let field = []
-    for (let j = 0; j < FIELD_DIM; j++) {
-        let row = []
-        for (let i = 0; i < FIELD_DIM; i++) {
-            const x = w * j
-            const y = w * i
-            const div = 200
-            let n = noise(x / div, y / div)
-            let angle = map(n, 0, 1, 0, 180)
-            row.push(angle)
-        }
-
-        field.push(row)
-    }
-
-    return field
-}
 
 function setup() {
     let dim = min(windowWidth, windowHeight)
     createCanvas(dim, dim);
     angleMode(DEGREES)
-    w = width / FIELD_DIM
-    angleField = makeAngleField()
-    for (let i = 0; i < numParticles; i++) {
-        particles.push(new Particle(random(width), random(height), 20))
-    }
 }
 
 function draw() {
     background(220);
-    for (let i = 0; i < angleField.length; i++) {
-        for (let j = 0; j < angleField[0].length; j++) {
-            const x = w * i
-            const y = w * j
-            let angle = floor(angleField[i][j])
-            // rect(x, y, w)
-
-            push()
-            translate(x + w / 2, y + w / 2)
-            rotate(angle)
-            line(0, 0, 10, 0)
-            pop()
-        }
-    }
-
-    for (let p of particles) {
-        p.render()
-        p.setVel(angleField)
-        p.update()
-    }
 }
 
 function windowResized() {
