bfa59303f9148bb11e8ebd9a41e3d36470e3c71c
jsc-264 <joestephen264@gmail.com>
2026-09-05 15:21:19 +0100

    flow: removed setVel method

--- flow/Particle.js
+++ flow/Particle.js
@@ -13,26 +13,11 @@ class Particle {
 
     update() {
         this.vel.add(this.acc)
-        this.vel.limit(4)
         this.pos.add(this.vel)
         this.acc.mult(0)
-
-        if (this.pos.x < 0) this.pos.x = width
-        if (this.pos.x > width) this.pos.x = 0
-        if (this.pos.y < 0) this.pos.y = height
-        if (this.pos.y > height) this.pos.y = 0
     }
 
-    push(vec){
+    force(vec){
         this.acc.add(vec)
     }
-
-    setVel(field) {
-        const i = floor((this.pos.x / w) % FIELD_DIM)
-        const j = floor((this.pos.y / w) % FIELD_DIM)
-        const angle = field[i][j]
-        const vec = p5.Vector.fromAngle(angle).setMag(10)
-        this.push(vec)
-    }
-
 }
