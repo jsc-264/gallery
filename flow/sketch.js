62fd44fac2828edb80e03c9a9d984d68e827ed48
jsc-264 <joestephen264@gmail.com>
2026-09-05 15:15:03 +0100

    flow: created perlin noise feild

--- flow/sketch.js
+++ flow/sketch.js
@@ -1,16 +1,27 @@
-const FIELD_DIM = 20
+const DIM = 50
+let res
 
 function setup() {
-    let dim = min(windowWidth, windowHeight)
-    createCanvas(dim, dim);
+    let minDim = min(windowWidth, windowHeight)
+    createCanvas(minDim, minDim);
     angleMode(DEGREES)
+    res = width / DIM
 }
 
 function draw() {
     background(220);
+
+    for (let y = 0; y < DIM; y++) {
+        for (let x = 0; x < DIM; x++) {
+            const n = noise(x/10, y/10)
+            const col = n * 255
+            fill(col)
+            rect(x * res, y * res, res)
+        }
+    }
 }
 
 function windowResized() {
-    let dim = min(windowWidth, windowHeight)
-    resizeCanvas(dim, dim);
+    let minDim = min(windowWidth, windowHeight)
+    resizeCanvas(minDim, minDim);
 }
