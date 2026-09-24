a00be19e6a17763b41e4a84f1a9634b532c31d67
jsc-264 <joestephen264@gmail.com>
2026-09-24 14:07:10 +0100

    spiral: show pic of parrot

--- spiral/sketch.js
+++ spiral/sketch.js
@@ -1,7 +1,14 @@
-function setup() {
-    createCanvas(800, 600);
+let img;
+
+async function setup() {
+    createCanvas(500, 500);
+    img = await loadImage("./assets/parrot.jpg")
+    img.resize(100, 100)
+    noSmooth()
 }
 
 function draw() {
     background(220);
+    image(img, 0, 0, width, height)
+
 }
